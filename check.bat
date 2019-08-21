11bd07241277003022aa01bbda3655b6bbcf1bcc
Guillaume M <guillaume.Melon@gmail.com>
2019-08-21 16:02:35 +0200

    Create check.bat

---
+++ check.bat
@@ -0,0 +1,10 @@
+@echo off
+::Test If script has Admin Priviledges/is elevated
+net session >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+ ECHO you are Administrator
+) ELSE (
+ ECHO you are NOT Administrator. Exiting...
+ PING 127.0.0.1 > NUL 2>&1
+ EXIT /B 1
+)
